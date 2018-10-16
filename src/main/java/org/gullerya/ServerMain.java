fe4d1b2abbf92b355810898d4d1162e65372921c
gullery <gullerya@gmail.com>
2018-10-16 18:30:21 +0300

    tech: added logger and server dependencies

---
+++ src/main/java/org/gullerya/ServerMain.java
@@ -0,0 +1,12 @@
+package org.gullerya;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ServerMain {
+	private static final Logger logger = LoggerFactory.getLogger(ServerMain.class);
+
+	public static void main(String[] args) {
+		logger.info("something");
+	}
+}
